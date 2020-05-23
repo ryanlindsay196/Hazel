035d59b163e8d114831db2f962b3ce5bc5f23972
rlindsa@masonlive.gmu.edu <rlindsa@masonlive.gmu.edu>
2020-05-23 16:22:46 -0400

    Finished Ep 23: ImGui Docking and Viewports: LINKER ERRORS PRESENT

--- Hazel/Sandbox/src/SandboxApp.cpp
+++ Hazel/Sandbox/src/SandboxApp.cpp
@@ -1,5 +1,7 @@
 #include <Hazel.h>
 
+#include "imgui/imgui.h"
+
 class ExampleLayer : public Hazel::Layer
 {
 public:
@@ -12,6 +14,13 @@ public:
 	{
 		if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
 			HZ_TRACE("Tab key is pressed!");
+	}
+
+	virtual void OnImGuiRender() override
+	{
+		ImGui::Begin("Test");
+		ImGui::Text("Hello World");
+		ImGui::End();
 	}
 
 	void OnEvent(Hazel::Event& event) override
@@ -31,7 +40,6 @@ public:
 	Sandbox()
 	{
 		PushLayer(new ExampleLayer());
-		PushOverlay(new Hazel::ImGuiLayer());
 	}
 
 	~Sandbox()
