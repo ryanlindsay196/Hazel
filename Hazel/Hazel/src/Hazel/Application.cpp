2eaf3570ea6a68c56cb82972f57af109df53c641
rlindsa@masonlive.gmu.edu <rlindsa@masonlive.gmu.edu>
2020-05-23 10:22:53 -0400

    Finished Ep 19: Input Polling

--- Hazel/Hazel/src/Hazel/Application.cpp
+++ Hazel/Hazel/src/Hazel/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <Glad/glad.h>
 
+#include "Input.h"
+
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -58,6 +60,9 @@ namespace Hazel {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto[x, y] = Input::GetMousePosition();
+			HZ_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
